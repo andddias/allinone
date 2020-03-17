c873add6ccaa7a282073628d34a6385265013fda
Andre Luiz <and.ddias@gmail.com>
2020-03-16 22:39:13 -0300

    Formulario de envio multiplos arquivos

--- cfoab/urls.py
+++ cfoab/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
-from .views import IndexView
+from .views import IndexView, FileFildView
 
 urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('', IndexView.as_view(), name='index'),
+    path('upload/', FileFildView.as_view(), name='upload'),
+    path('success/', FileFildView.as_view(), name='success'),
 ]
--- cfoab/views.py
+++ cfoab/views.py
@@ -1,6 +1,27 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
+from django.views.generic.edit import FormView
+from django.urls import reverse
+
+from .forms import FileFieldForm
 
 
 class IndexView(LoginRequiredMixin, TemplateView):
     template_name = 'index.html'
+
+
+class FileFildView(LoginRequiredMixin, FormView):
+    form_class = FileFieldForm
+    template_name = 'upload.html'
+    success_url = 'success'
+
+    def post(self, request, *args, **kwargs):
+        form_class = self.get_form_class()
+        form = self.get_form(form_class)
+        files = request.FILES.getlist('file_field')
+        if form.is_valid():
+            for f in files:
+                print(f)
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
